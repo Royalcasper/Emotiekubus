5c69b9ffb8ee25e3f8734124ed0162d3447f0f68
Casper <casperclaassen@gmail.com>
2019-01-19 22:35:59 +0100

    updated

--- js/cube.js
+++ js/cube.js
@@ -1,6 +1,10 @@
 //Global Variables cube positie onthouden
 let cubeX = 0;
 let cubeY = 0;
+
+//Voor de visualisatie van de cube.
+var xAngle = 0, yAngle = 0;
+
 //De data van de json beschikbaar als global variable
 var emotionsData;
 
@@ -12,7 +16,7 @@ var jelous;
 var zen;
 var confused;
 
-//De audio library van elke emotie beschikbaar als
+//De audio library van elke emotie beschikbaar als global variable
 var happyvoice;
 var sadvoice;
 var angryvoice;
@@ -20,8 +24,10 @@ var jelousvoice;
 var zenvoice;
 var confused;
 
+//Een variabele bedoeld om de actieve emotie door te geven.
 var activeEmotion = happyvoice;
 
+//Deze methode haalt alle data op uit de json en passeert de data door naar een aantal global variables.
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -47,21 +53,31 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open("GET", "data/emotions.json", true);
 xmlhttp.send();
 
+//luister naar een keydown event als een van de keys overeenkomt met de code dan wordt de code erbinnen uitgevoert
 document.addEventListener('keydown', function (a) {
+	//Pijltje naar links
     if (a.which == 37) {
     	leftArrowPressed();
+    	yAngle -= 90;
     }
+    //pijltje naar rechts
      if (a.which == 39) {
     	rightArrowPressed();
+    	yAngle += 90;
     }
+    //pijltje naar boven
      if (a.which == 38) {
     	upArrowPressed();
+    	xAngle += 90;
     }
+    //pijltje naar onder
      if (a.which == 40) {
     	downArrowPressed();
-    }
+    	xAngle -= 90;
+    };
+    document.getElementById('cube').style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
 }, false);
-
+//Deze functie haalt -1 af van de cubeX variable als hij 0 heeft berijkt gaat hij weer terug naar 3. Vervolgens wordt de get emotion functie uitgevoert.
 function leftArrowPressed() {
     if (cubeX <= 0) {
         cubeX = 3;
@@ -73,7 +89,7 @@ function leftArrowPressed() {
         console.log(cubeX);
     }
 }
-
+//Deze functie voegt +1 toe aan de cubeX als hij 3 heeft berijkt gaat hij weer terug naar 0. Vervolgens wordt de get emotion functie uitgevoert.
 function rightArrowPressed() {
     if (cubeX >= 3) {
         cubeX = 0;
@@ -85,7 +101,7 @@ function rightArrowPressed() {
         console.log(cubeX);
     }
 }
-
+//Deze functie voegt +1 toe aan de cubeY als hij 3 heeft berijkt gaat hij weer terug naar 0. Vervolgens wordt de get emotion functie uitgevoert.
 function upArrowPressed() {
     if (cubeY >= 3) {
         cubeY = 0;
@@ -97,7 +113,7 @@ function upArrowPressed() {
         console.log(cubeY);
     }
 }
-
+//Deze functie haalt -1 af van de cubeX variable als hij 0 heeft berijkt gaat hij weer terug naar 3. Vervolgens wordt de get emotion functie uitgevoert.
 function downArrowPressed() {
     if (cubeY <= 0) {
         cubeY = 3;
@@ -110,38 +126,47 @@ function downArrowPressed() {
     }
 }
 
+//Timer voor het afspelen van audio. Bleek niet helemaal goed te werken dus heb hem geschrapt.
+
 // function voiceTimer()
 // {
 // 	var timer = Math.floor(Math.random()*(15000-4000+1)+4000);
 // 	setTimeout(playVoice(),timer);
 // 	console.log(timer);
 // }
+//
 
+//De get emotion functie kijkt naar value van de cubeX en cubeY en voert vervolgens de bijbehorende if statement uit.
 function getEmotion() {
+	//Veranderd de image naar die van de happy emotie. Ook voert hij de getvoice funtie uit en geeft de happyvoice array mee. Daarnaast veranderd hij ook de status van de actieve emotie deze funtie is deels geschrapt.
     if (cubeX == 0 && cubeY == 0) {
         image = document.getElementById('faceDisp');
         image.src = happy.face;
         getVoice(happyvoice);
         activeEmotion = happyvoice;
     }
+    //Veranderd de image naar die van de zen emotie. Ook voert hij de getvoice funtie uit en geeft de zenvoice array mee. Daarnaast veranderd hij ook de status van de actieve emotie deze funtie is deels geschrapt.
     if (cubeX == 1 && cubeY == 0) {
         image = document.getElementById('faceDisp');
         image.src = zen.face;
         getVoice(zenvoice);
         activeEmotion = zenvoice;
     }
+     //Veranderd de image naar die van de jelous emotie. Ook voert hij de getvoice funtie uit en geeft de jelousvoice array mee. Daarnaast veranderd hij ook de status van de actieve emotie deze funtie is deels geschrapt.
     if (cubeX == 2 && cubeY == 0) {
         image = document.getElementById('faceDisp');
         image.src = jelous.face;
         getVoice(jelousvoice);
         activeEmotion = jelousvoice;
     }
+     //Veranderd de image naar die van de confused emotie. Ook voert hij de getvoice funtie uit en geeft de confusedvoice array mee. Daarnaast veranderd hij ook de status van de actieve emotie deze funtie is deels geschrapt.
     if (cubeX == 3 && cubeY == 0) {
         image = document.getElementById('faceDisp');
         image.src = confused.face;
         getVoice(confusedvoice);
         activeEmotion = confusedvoice;
     }
+     //Veranderd de image naar die van de sad emotie. Ook voert hij de getvoice funtie uit en geeft de sadvoice array mee. Daarnaast veranderd hij ook de status van de actieve emotie deze funtie is deels geschrapt.
     if (cubeX == 0 && cubeY == 1) {
         image = document.getElementById('faceDisp');
         image.src = sad.face;
@@ -154,6 +179,7 @@ function getEmotion() {
         getVoice(sadvoice);
         activeEmotion = sadvoice;
     }
+	 //Veranderd de image naar die van de sad emotie. Ook voert hij de getvoice funtie uit en geeft de sadvoice array mee. Daarnaast veranderd hij ook de status van de actieve emotie deze funtie is deels geschrapt.
     if (cubeX == 2 && cubeY == 1) {
         image = document.getElementById('faceDisp');
         image.src = angry.face;
@@ -216,58 +242,61 @@ function getEmotion() {
     }
 }
 
-function playVoice(selectedvoice) {
-    var voice = new Audio(selectedvoice);
-    voice.play();
-}
-
+//pak van de array die is binnengekomen een random voice uit de array en stuur deze door naar playvoice.
 function getVoice(emotionvoice) {
     var rand = emotionvoice[Math.floor(Math.random() * emotionvoice.length)];
     playVoice(rand);
 }
 
-var sound;
-var amp;
-var canvas;
-var button;
-
-function toggleSound() {
-    if (sound.isPlaying()) {
-        sound.pause();
-    } else {
-        sound.play();
-    }
+//speel de audio af die is binnen gekomen.
+function playVoice(selectedvoice) {
+    var voice = new Audio(selectedvoice);
+    voice.play();
 }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
 
-function preload() {
-    sound = loadSound("audio/sad/sad1.mp3");
-}
+// var sound;
+// var amp;
+// var canvas;
+// var button;
 
-function setup() {
-    button = createButton('toggle');
-    button.mousePressed(toggleSound);
-    canvas = createCanvas(windowWidth, windowHeight);
-    canvas.position(0, 0);
-    canvas.style('z-index', '-1');
-    amp = new p5.Amplitude();
-}
+// function toggleSound() {
+//     if (sound.isPlaying()) {
+//         sound.pause();
+//     } else {
+//         sound.play();
+//     }
+// }
 
-function draw() {
-    // put drawing code here
-    background(175);
-    var vol = amp.getLevel();
-    ellipse(width / 2, height / 2, vol * 500);
-    // currentVoice = playVoice(selectedvoice);
-    // if (activeEmotion == currentVoice) {
+// function windowResized() {
+//     resizeCanvas(windowWidth, windowHeight);
+// }
 
-    // } 	
-    //   function playVoice(selectedvoice) 
-    // {
-    // 	sound = loadSound(selectedvoice);			
-    // 	sound.play();
-    // }
-}
+// function preload() {
+//     sound = loadSound("audio/sad/sad1.mp3");
+// }
+
+// function setup() {
+//     button = createButton('toggle');
+//     button.mousePressed(toggleSound);
+//     canvas = createCanvas(windowWidth, windowHeight);
+//     canvas.position(0, 0);
+//     canvas.style('z-index', '-2');
+//     amp = new p5.Amplitude();
+// }
+
+// function draw() {
+//     // put drawing code here
+//     background(175);
+//     var vol = amp.getLevel();
+//     ellipse(width / 2, height / 2, vol * 500);
+//     // currentVoice = playVoice(selectedvoice);
+//     // if (activeEmotion == currentVoice) {
+
+//     // } 	
+//     //   function playVoice(selectedvoice) 
+//     // {
+//     // 	sound = loadSound(selectedvoice);			
+//     // 	sound.play();
+//     // }
+// }
